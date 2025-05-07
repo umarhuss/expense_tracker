0f47bb948dd7ffbf6c08dc358cfbe942edb4f8fe
Umar <umar@mac.home>
2025-05-07 16:57:10 +0100

    Added initial dashbaord layout with placeholders

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 # Create an instance of the Flask class
 # Template folder tells flask where to look for templates
-app = Flask(__name__, template_folder="app/templates")
+app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
 
 
 # Use @app to render pages it is a decorator so a function must go below
